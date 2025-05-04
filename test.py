b68acca7e9fa0ac6881e561c021116585167afd2
fazapoint <fazaramadhani99@gmail.com>
2025-05-04 22:38:00 +0700

    contine course

---
+++ test.py
@@ -0,0 +1,14 @@
+import turtle
+
+wn = turtle.Screen() # create wn as instance of screen class
+alex = turtle.Turtle() # create alex as instance of turtle class
+
+alex.forward(150)
+alex.left(90) # turn 90 degrees to left
+alex.forward(75)
+alex.left(90)
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+
+turtle.done() # keep graphic window open
